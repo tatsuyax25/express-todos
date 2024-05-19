1a8a423b1d9a38aa1810469eaa173261164dab50
Miguel Urena <urena_miguel82@outlook.com>
2024-05-19 11:53:49 -0700

    Fixing crash issue

--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,17 @@
 let express = require('express');
 let router = express.Router();
+// Require the Todo model
+const todoDb = require('../models/todo');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+app.get('/todos', function (req, res) {
+  res.render("todos/index", {
+    todos: todoDb.getAll(),
+  });
 });
 
 module.exports = router;
